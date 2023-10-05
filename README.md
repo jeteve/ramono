42e3cebe9d5750fd788e56b23518222c35e5c377
Jerome Eteve <jeteve@users.noreply.github.com>
2023-10-05 21:18:07 +0000

    Minimum version, with memory consumption

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ramono
+
+```ramono``` is a small program that consumes resources
+
+# Run it
+
+This is meant to be made available as a docker image.
+
+# Notes
+
+Compiling for alpine and other MUSL based distribs (see https://musl.libc.org/)
+
+```
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::{time::Duration, thread};
 
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct CliArg{
+    #[clap(short = 'm', long = "mem-increment", default_value_t = 1000)]
+    memory_increment: usize,
+    #[clap(short = 'l', long = "mem-limit", default_value_t = 1000 * 1000 * 1000 )]
+    memory_limit: usize
+}
+
 fn main() {
     env_logger::Builder::new()
         .target(env_logger::Target::Stdout)
@@ -7,8 +17,22 @@ fn main() {
         .init();
 
     log::info!("Starting Ramono");
+
+    let args = CliArg::parse();
+
+
+
+    let memory_increment = args.memory_increment;
+    let memory_limit = args.memory_limit;
+
     let one_second = Duration::from_secs(1);
+
+    let mut mem_vector: Vec<u8> = vec![0; 0];
     loop{
         thread::sleep(one_second);
+        if mem_vector.len() <= memory_limit {
+            mem_vector.resize(mem_vector.len() + memory_increment , 0);
+        }
+        log::info!("Vector len={}B cap={}B", mem_vector.len(), mem_vector.capacity());
     }
 }
